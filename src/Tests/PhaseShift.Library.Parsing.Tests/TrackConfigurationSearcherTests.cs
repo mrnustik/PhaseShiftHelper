6d8cabc40ed2b97ba9116e156374f609e194e347
Michal Mrnuštík <michal.mrnustik@outlook.com>
2019-05-28 22:48:50 +0200

    Add track configuration searching

---
+++ src/Tests/PhaseShift.Library.Parsing.Tests/TrackConfigurationSearcherTests.cs
@@ -0,0 +1,22 @@
+﻿using PhaseShift.Library.Parsing.Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace PhaseShift.Library.Parsing.Tests
+{
+    public class TrackConfigurationSearcherTests
+    {
+        [Fact] 
+        public void Find_InParentDirectory_HasValidValues()
+        {
+            var sut = new TrackConfigurationSearcher();
+
+            var configurationFiles = sut.FindSongConfigurationFiles("../../../../../../sample_data");
+
+            Assert.Equal(2, configurationFiles.Count());
+        }
+    }
+}
