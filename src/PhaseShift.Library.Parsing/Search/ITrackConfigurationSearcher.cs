39b30b717c877d7b36f95386f1d9048707ef2eba
Michal Mrnuštík <michal.mrnustik@outlook.com>
2019-05-28 22:48:50 +0200

    Add track configuration searching

---
+++ src/PhaseShift.Library.Parsing/Search/ITrackConfigurationSearcher.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace PhaseShift.Library.Parsing.Search
+{
+    public interface ITrackConfigurationSearcher
+    {
+        IEnumerable<FileInfo> FindSongConfigurationFiles(string libraryPath);
+    }
+}
---
+++ src/PhaseShift.Library.Parsing/Search/TrackConfigurationSearcher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PhaseShift.Library.Parsing.Search
+{
+    public class TrackConfigurationSearcher : ITrackConfigurationSearcher
+    {
+        public IEnumerable<FileInfo> FindSongConfigurationFiles(string libraryPath)
+        {
+            var libraryDirectory = new DirectoryInfo(libraryPath);
+            return FindSongConfigurationsInFolder(libraryDirectory);
+        }
+
+        private IEnumerable<FileInfo> FindSongConfigurationsInFolder(DirectoryInfo directory)
+        {
+            var result = new List<FileInfo>();
+            foreach (var entry in directory.EnumerateFileSystemInfos())
+            {
+                if (IsDirectory(entry))
+                {
+                    result.AddRange(FindSongConfigurationsInFolder(new DirectoryInfo(entry.FullName)));
+                }
+                else
+                {
+                    if (IsConfigurationFile(entry))
+                    {
+                        result.Add(new FileInfo(entry.FullName));
+                    }
+                }
+            }
+            return result;
+        }
+
+        private bool IsDirectory(FileSystemInfo entry)
+        {
+            return (entry.Attributes & FileAttributes.Directory) != 0;
+        }
+
+        private bool IsConfigurationFile(FileSystemInfo entry)
+        {
+            return entry.Name == "song.ini";
+        }
+    }
+}
